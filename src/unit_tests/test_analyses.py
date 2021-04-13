de6c51a9c05967af481eb8d43669e826dd82729e
AlexPasqua <alexpasquali98@gmail.com>
2021-04-13 20:57:30 +0200

    Added num_administrations_per_category to analyses.py

--- src/unit_tests/test_analyses.py
+++ src/unit_tests/test_analyses.py
@@ -6,6 +6,9 @@ class TestFunctions(unittest.TestCase):
     def test_num_vaccinated_by_age(self):
         num_vaccinated_by_age("../../covid19-opendata-vaccini/dati", "../../output_files/")
 
+    def test_num_vaccinated_per_category(self):
+        num_administrations_per_category("../../covid19-opendata-vaccini/dati", "../../output_files/")
+
 
 if __name__ == '__main__':
     unittest.main()
